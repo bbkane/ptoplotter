54ac6d928311e1b2e0c6a569b4b849b1ac9c3510
Benjamin Kane <bbk1524@gmail.com>
2017-12-27 13:12:19 -0600

    YAML working
    Need to get the graph working now

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var myCodeMirror = CodeMirror(document.body, {
-  value: "a: 1",
-  mode:  "yaml"
+let editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  lineNumbers: true,
+  tabMode: "indent"
+});
+
+editor.on('change', function(cm) {
+  let doc = jsyaml.load(cm.getValue());
+  console.log(doc);
 });
