e5ef46689b710dfb1ceaded8c63755122e41bd0a
Benjamin Kane <bbk1524@gmail.com>
2017-12-27 14:45:08 -0600

    Add basic plot

--- index.js
+++ index.js
@@ -5,5 +5,48 @@ let editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 
 editor.on('change', function(cm) {
   let doc = jsyaml.load(cm.getValue());
-  console.log(doc);
+  plotlyJSON = docToPlotlyJSON(doc);
+  Plotly.newPlot('graphdiv', data);
 });
+
+// https://stackoverflow.com/a/17727953/2958070
+function daysBetween(start_date, end_date) {
+  start_utc = Date.UTC(start_date.getFullYear(), start_date.getMonth(), start_date.getDate())
+  end_utc = Date.UTC(end_date.getFullYear(), end_date.getMonth(), end_date.getDate())
+  return (end_date - start_date) / 86400000;
+}
+
+function docToPlotlyJSON(doc) {
+  // Collect all traces (see https://plot.ly/javascript/line-charts/)
+  data = []
+
+  // https://stackoverflow.com/a/10040679/2958070
+  let start_date = doc['start_date'];
+  let end_date = doc['end_date'];
+  let days_between = daysBetween(start_date, end_date);
+
+  // all of these plots are going to share x-axises...
+  let days = [];
+
+  // am I double allocating here?
+  for (let d = new Date(start_date); d <= end_date; d.setDate(d.getDate() + 1)) {
+    days.push(new Date(d));
+  }
+
+  // Lets start with hour_markers
+  let hour_markers = []
+  for(let hr of doc['hour_markers']) {
+    let hr_change = Array.apply(null, Array(days_between)).map(Number.prototype.valueOf,hr);
+    hour_markers.push(hr_change);
+  }
+
+  for (let ys of hour_markers) {
+    data.push( {
+      x: days,
+      y: ys,
+      type: 'scatter'
+    });
+  }
+
+  return data;
+}
