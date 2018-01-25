b3c958a9aa05fa51696fe381da50593b7f8b07aa
Benjamin Kane <bbk1524@gmail.com>
2018-01-25 06:59:35 -0600

    Start adding webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   // https://stackoverflow.com/a/33374807/2958070
   plugins: [
     new CopyWebpackPlugin([
       { from: 'src/static' }
-    ])
+    ]),
+    // new UglifyJSPlugin()  // TODO: this takes a while so guard it behind https://stackoverflow.com/a/31228568/2958070
   ],
   // From the README
   module: {
