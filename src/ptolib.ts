1f6b164417e4ece2ba756e911fab9e3adaa1e833
Benjamin Kane <bbk1524@gmail.com>
2018-05-02 09:26:21 -0500

    Delete old code

--- src/ptolib.ts
+++ src/ptolib.ts
@@ -1,5 +1,3 @@
-import { randomBytes } from "crypto";
-
 // Generated from https://app.quicktype.io/
 // then modified to support Dates instead of strings
 
@@ -62,6 +60,11 @@ export function makeSequentialDateArray(startDate: Date, endDate: Date): Date[] 
     return days;
 }
 
+function makeFilledArray(length, value) {
+    let arr = Array.apply(null, Array(length)).map(Number.prototype.valueOf, value);
+    return arr
+}
+
 // TODO: make this configurable
 function isWeekDay(date: Date): boolean {
     return date.getDay() >= 1 && date.getDay() <= 6
@@ -82,7 +85,6 @@ export function normalizeDeltas(holidays: Date[], currentDay: Date, deltas: numb
     // Always apply positive changes
     // Only apply a negative change if it's not a weekend or holiday
     // Take max -8 negative hours
-    // let deltas: number[] = [repeatingChangeDelta, oneDayChangeDelta, rangedChangeDelta];
 
     if (deltas.every(x => x == 0)) {
         return 0;
@@ -118,7 +120,6 @@ export function docToInterestingDates(doc: EditorInfo, allDays: Date[]): Map<Dat
 
         let deltas: number[] = []
 
-        // only count Monday-Friday (TODO: make this a flag in the input somewhere?)
         for (let rangedChange of doc.ranged_changes) {
             if (currentDay.getTime() >= rangedChange.start_date.getTime() &&
                     currentDay.getTime() <= rangedChange.end_date.getTime()) {
@@ -135,7 +136,6 @@ export function docToInterestingDates(doc: EditorInfo, allDays: Date[]): Map<Dat
         }
 
         for (let oneDayChange of doc.one_day_changes) {
-            // TODO: getTime isn't a function?
             if (currentDay.getTime() == oneDayChange.date.getTime()) {
                 deltas.push(oneDayChange.hour_change);
                 break;
@@ -157,8 +157,7 @@ export function makePlots(doc: EditorInfo, allDays: Date[], interestingDates: Ma
     let balances : number[] = [];
     let positiveDays: Date[] = [];
     let negativeDays: Date[] = [];
-    // NOTE: I think I'm going to change around the holidays
-    // instead of screwing around with sentinals
+
     let balance = doc.start_hours;
 
     for (let currentDay of allDays) {
@@ -224,151 +223,4 @@ export function makePlots(doc: EditorInfo, allDays: Date[], interestingDates: Ma
     }
 
     return plots;
-}
-
-// OLD STUFF
-
-function makeFilledArray(length, value) {
-    let arr = Array.apply(null, Array(length)).map(Number.prototype.valueOf, value);
-    return arr
-}
-
-// https://stackoverflow.com/a/17727953/2958070
-function getDaysBetween(start_date: Date, end_date: Date): number {
-    let start_utc = Date.UTC(start_date.getFullYear(), start_date.getMonth(), start_date.getDate())
-    let end_utc = Date.UTC(end_date.getFullYear(), end_date.getMonth(), end_date.getDate())
-    return (end_utc - start_utc) / 86400000;
-}
-
-// Needs a {positive: [], neutral: [], negative: []}
-// an int
-// and a date (This function will copy the date if necessary)
-function addToCorrectMarkerDates(marker_dates, hour_change, date) {
-    let change = 'positive';
-    if (hour_change < 0) {
-        change = 'negative';
-    }
-    if (hour_change === 0) {
-        change = 'neutral';
-    }
-    marker_dates[change].push(new Date(date));
-}
-
-// This takes the code_editor -> JSON
-export function docToPlotlyJSONOld(doc): Plot[] {
-    // Collect all traces (see https://plot.ly/javascript/line-charts/)
-    let data = []
-
-    // https://stackoverflow.com/a/10040679/2958070
-    let start_date = doc['start_date'];
-    let end_date = doc['end_date'];
-    let days_between = getDaysBetween(start_date, end_date);
-
-    // most of these plots are going to share x-axes...
-    let days = [];
-
-    let ys = []; // This keeps track of the PDO balance per day from start_date to end_date inclusive
-    let hours = doc['start_hours'];
-
-
-    let marker_dates = {
-        'positive': [],
-        'neutral': [],
-        'negative': []
-    };
-
-    let has_repeating_changes = ('repeating_changes' in doc);
-    let has_one_day_changes = ('one_day_changes' in doc);
-    let has_ranged_changes = ('ranged_changes' in doc);
-
-    // TODO: make this not O(n(a + b + c + ...))
-    for (let d = new Date(start_date); d <= end_date; d.setDate(d.getDate() + 1)) {
-        days.push(new Date(d.getTime()));
-
-        if (has_repeating_changes) {
-            for (let obj of doc['repeating_changes']) {
-                if (obj.day_of_month === d.getDate()) {
-                    hours += obj.hour_change;
-                    addToCorrectMarkerDates(marker_dates, obj.hour_change, d);
-                }
-            }
-        }
-
-        if (has_one_day_changes) {
-            for (let obj of doc['one_day_changes']) {
-                // http://adripofjavascript.com/blog/drips/checking-date-equality-in-javascript.html
-                if (obj.date.getTime() === d.getTime()) {
-                    hours += obj.hour_change;
-                    addToCorrectMarkerDates(marker_dates, obj.hour_change, d);
-                }
-            }
-        }
-
-        if (has_ranged_changes) {
-            for (let obj of doc['ranged_changes']) {
-                let d_time = d.getTime();
-                if (d.getDay() >= 1 && d.getDay() <= 6 && // weekday
-                    obj.start_date.getTime() <= d_time &&
-                    d_time <= obj.end_date.getTime()) {
-                    hours += obj.hour_change;
-                    addToCorrectMarkerDates(marker_dates, obj.hour_change, d);
-                }
-            }
-        }
-
-        ys.push(hours);
-    }
-
-    // Add the PDO trace
-    data.push({
-        x: days,
-        y: ys,
-        mode: 'scatter',
-        name: 'PDO'
-    });
-
-    // Add the markers
-    data.push({
-        x: marker_dates.negative,
-        y: makeFilledArray(marker_dates.negative.length, 0),
-        mode: 'markers',
-        name: 'losses',
-        marker: {
-            color: 'red'
-        }
-    });
-
-    data.push({
-        x: marker_dates.neutral,
-        y: makeFilledArray(marker_dates.neutral.length, 0),
-        mode: 'markers',
-        name: 'no change',
-        marker: {
-            color: 'blue'
-        }
-    });
-
-    data.push({
-        x: marker_dates.positive,
-        y: makeFilledArray(marker_dates.positive.length, 0),
-        mode: 'markers',
-        name: 'gains',
-        marker: {
-            color: 'green'
-        }
-    });
-
-    // Add all hour_markers
-    let hour_markers = []
-    for (let obj of doc['hour_markers']) {
-        let hr_change = makeFilledArray(days_between, obj.hours);
-        data.push({
-            x: days,
-            y: hr_change,
-            mode: 'scatter',
-            name: obj.label
-        });
-    }
-
-    return data;
 }
